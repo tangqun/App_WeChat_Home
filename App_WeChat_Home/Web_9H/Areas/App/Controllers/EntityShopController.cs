60ba4e451d1bb544ed5ae3e50948928844a9984d
TQ <TQ@DESKTOP-NF8QRJ1>
2017-06-05 09:11:42 +0800

    fix

---
+++ App_WeChat_Home/Web_9H/Areas/App/Controllers/EntityShopController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web_9H.Areas.App.Controllers
+{
+    public class EntityShopController : Controller
+    {
+        // GET: App/EntityShop
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
